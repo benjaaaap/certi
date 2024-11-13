d41ef06eb432e1ccfa8875930cd1cde1812e85db
benjaaaap <benjamin.poblete.donoso@alumnos.sip.cl>
2024-11-13 12:04:42 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,13 @@
+document.getElementById("donar").addEventListener("click", function() {
+    this.style.display="none";
+});
+
+
+function mostrar() {
+    var div = document.getElementById('container');
+    div.style.display = 'block'; // Cambia el display a 'block' para mostrar el div
+  }
+  function ocultar() {
+    var div = document.getElementById('container');
+    div.style.display = 'none';
+  }
